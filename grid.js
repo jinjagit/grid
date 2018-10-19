1f1bdf75772edae3e58f3a4e5b936f7155cfadb5
Simon Tharby <jinjamail@gmail.com>
2018-10-19 14:10:34 +0100

    redraw grid less, or not at all (depending on PC speed vs. interval setting) while window resizing

--- grid.js
+++ grid.js
@@ -2,12 +2,15 @@
 
 function drawPage() {
   windowH = window.innerHeight;
+  lastWindowH = windowH;
   cellHeight = windowH / gridN;
   borderW = windowH / 400;
   menuBoxH = windowH / 20;
 
   sidebarDiv.style.display = "none";
+  waitMsg.style.display = "none";
   body.style.backgroundColor = backgroundColor;
+  grid.style.display = "grid";
   grid.style.height = `${windowH}px`;
   grid.style.backgroundColor = backgroundColor;
   grid.style.gridGap = gridGap;
@@ -15,11 +18,12 @@ function drawPage() {
   destroyCells();
   drawCells();
 
-
+  console.log("page redrawn");
+  queueRedraw = true;
 }
 
 function drawMenu() {
-  lastWindowH = windowH;
+  lastMenuH = windowH;
   sidebarTrigger.style.width = `${windowH / 100}px`;
   sidebarDiv.style.width = `${menuBoxH * 2.5}px`;
   titleBox.style.height = `${menuBoxH * 2}px`;
@@ -29,7 +33,7 @@ function drawMenu() {
 
   gridBtnText.style.lineHeight = `${menuBoxH}px`;
   gridBtnText.style.fontSize = `${menuBoxH / 2}px`;
-  console.log("redrawn");
+  console.log("menu redrawn");
 }
 
 function drawCells() {
@@ -57,13 +61,36 @@ function destroyCells() {
   }
 }
 
+function redrawPage() {
+  if (queueRedraw == true) {
+    body.style.backgroundColor = "black";
+    waitMsg.style.display = "block";
+    waitMsg.style.margin = `${menuBoxH * 10}px 0 0 0`;
+    waitMsg.style.textAlign = "center";
+    grid.style.display = "none";
+    delay = setInterval(waitBeforeRedraw, 75); // can be tweaked to speed of PC
+    queueRedraw = false;
+  }
+}
+
+function waitBeforeRedraw() {
+  windowH = window.innerHeight;
+
+  if (windowH != lastWindowH) {
+    lastWindowH = windowH;
+  } else {
+    clearInterval(delay);
+    drawPage();
+  }
+}
+
 function cellClick(clickedID) {
 
 }
 
 function triggerHover() {
   sidebarOn = true;
-  if (windowH != lastWindowH) {
+  if (windowH != lastMenuH) {
     drawMenu();
   }
   sidebarDiv.style.display = "inline-block";
@@ -94,10 +121,12 @@ function windowUnHover() {
 
 function clickGrid() {
   console.log("clicked");
+
 }
 
 let windowH = 0;
 let lastWindowH = 0;
+let lastMenuH = 0;
 
 let backgroundColor = "hsl(128, 30%, 40%)";
 let menuHoverColor = "hsl(128, 30%, 65%)";
@@ -108,12 +137,17 @@ let cellHeight = 0;
 let gridGap = "1px";
 let borderW = 0;
 
+let queueRedraw = false;
 let sidebarOn = false;
 
 let body = document.getElementsByTagName('body')[0];
 
 body.style.fontFamily = "'Ubuntu Mono', monospace";
 body.style.fontWeight = "normal";
+
+waitMsg.style.color = backgroundColor;
+waitMsg.style.display = "block";
+waitMsg.style.textAlign = "center";
 
 grid.style.display = "grid";
 sidebarTrigger.style.position = "absolute";
